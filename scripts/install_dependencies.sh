47cb270db898bae5223bd659b8dcb7e21d063b9b
Darshan Raval <Darshan@codepiemedia.com>
2019-04-11 11:25:46 +0530

    a1

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+apt -y update
+apt install -y nodejs
+apt install -y npm
+npm install -y pm2 -g
---
+++ scripts/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pwd
+cd /home/ubuntu
+git clone https://github.com/Darshannraval/nodejs.git
+cd /home/ubuntu/nodejs
+pm2 start app.js 
+
