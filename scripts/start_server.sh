420a17071129d9cacbdc14cb98dce71f6a04bd96
Darshan Raval <Darshan@codepiemedia.com>
2019-04-11 12:03:57 +0530

    a5

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-pwd
-cd /home/ubuntu
-git clone https://github.com/Darshannraval/test.git
 cd /home/ubuntu/test
 pm2 start -f app.js 
 
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,7 +3,10 @@ apt -y update
 apt install -y nodejs
 apt install -y npm
 npm install -y pm2 -g
+cd /home/ubuntu
+git clone https://github.com/Darshannraval/test.git
 cd /home/ubuntu/test
+pm2 stop -f app.js
 
 
 
