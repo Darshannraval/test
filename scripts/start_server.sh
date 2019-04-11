68d58959581e9a725e371bd0b823ad71944614cb
Darshan Raval <Darshan@codepiemedia.com>
2019-04-11 11:38:52 +0530

    a3

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,5 @@ pwd
 cd /home/ubuntu
 git clone https://github.com/Darshannraval/nodejs.git
 cd /home/ubuntu/nodejs
-pm2 start app.js 
+pm2 start -f app.js 
 
---
+++ scripts/stop_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/nodejs
+pm2 stop -f app.js 
